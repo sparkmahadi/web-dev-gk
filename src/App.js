c7b63971476cb2cc40fc6cae6655f3681ad6b564
sparkmahadi <mahadi.developer@gmail.comm>
2022-10-10 21:52:59 +0600

    added routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+import Main from './layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+        {
+          path: '/topics',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
+    }
+  ])
   return (
     <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
+import Nav from '../components/Nav/Nav';
+
+const Main = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
